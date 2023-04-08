32df1acb73b41eeeaccc304938796075be583bae
Daria <noyyt@mail.ru>
2023-04-08 17:52:00 +0200

    generated spring app

---
+++ webtech/src/main/java/Htwberlin/webtech/WebTechProjektSoSe2023Application.java
@@ -0,0 +1,13 @@
+package Htwberlin.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechProjektSoSe2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTechProjektSoSe2023Application.class, args);
+	}
+
+}
---
+++ webtech/src/test/java/Htwberlin/webtech/WebTechProjektSoSe2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package Htwberlin.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechProjektSoSe2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
