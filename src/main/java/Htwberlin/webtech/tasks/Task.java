e74c4ba98bc443806135eec78c1a8ca37a9c4d7c
Daria <noyyt@mail.ru>
2023-05-02 13:03:05 +0200

    Entity class Tasks was added.

---
+++ src/main/java/Htwberlin/webtech/tasks/Task.java
@@ -0,0 +1,91 @@
+package Htwberlin.webtech.tasks;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate deadline;
+    private String description;
+
+    public Task() {}
+    public Task(String name, LocalDate deadline, String description) {
+
+        this.name=name;
+        this.deadline=deadline;
+        this.description=description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+
+        if (!Objects.equals(id, task.id)) return false;
+        if (!Objects.equals(name, task.name)) return false;
+        if (!Objects.equals(deadline, task.deadline)) return false;
+        return Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (deadline != null ? deadline.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", deadline=" + deadline +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
